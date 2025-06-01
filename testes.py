b5053163625a0579cfad244459086305ba23e79d
Malujoro <mateus_rocha_sousa@hotmail.com>
2025-06-01 16:17:59 -0300

    Execução dos testes

--- testes.py
+++ testes.py
@@ -5,31 +5,13 @@ import time
 import csv
 import tracemalloc
 
-
-# if (__name__ == '__main__'):
-#     n = 8
-
-#     tempo_comeco = time.perf_counter()
-#     existe, tabuleiro = rodar_n_rainhas(n)
-#     tempo_fim = time.perf_counter()
-
-#     if (not existe):
-#         print('A solução não existe!\n')
-
-#     tempo = tempo_fim - tempo_comeco
-
-#     Desenho().desenhar_tabuleiro(tabuleiro, show=True)
-
-#     print(f'Tempo de execucao: {tempo:.6f} segundos')
-
-
 def linha(char: str = "=", tam: int = 70):
     print(char * tam)
 
 
 with open("tempos_rainhas.csv", mode="w", newline="") as arquivoCSV:
     writer = csv.writer(arquivoCSV)
-    writer.writerow(["rainhas", "algoritmo", "iteracao", "tempo", "memoria_pico_b",
+    writer.writerow(["rainhas", "algoritmo", "iteracao", "tempo", "memoria_pico_kb",
                     "total_conflitos", "rainhas_conflitos", "conflitos_linha", "conflitos_diagonais",])
 
     random.seed(2025)
@@ -37,8 +19,6 @@ with open("tempos_rainhas.csv", mode="w", newline="") as arquivoCSV:
     lista_rainhas = [10, 25, 26, 100, 250]
     limite_backtracking = lista_rainhas[2]
     desenho = Desenho(folder="imagens")
-
-    lista_rainhas = [250]
 
     for rainha in lista_rainhas:
         linha()
@@ -74,11 +54,9 @@ with open("tempos_rainhas.csv", mode="w", newline="") as arquivoCSV:
                 tracemalloc.stop()
 
                 tempo = fim - inicio
-                print(
-                    f"[{it + 1}ª Iteração] {tempo:.6f} segundos, com pico de {memoria_pico_b:.2f} bytes")
 
                 csv_tempo = f"{tempo:.6f}"
-                csv_memoria_pico = f"{memoria_pico_b:.2f}"
+                csv_memoria_pico = f"{memoria_pico_b / 1024:.2f}"
 
                 if (resultado):
                     csv_total_conflitos = resultado['total_conflitos']
@@ -90,6 +68,9 @@ with open("tempos_rainhas.csv", mode="w", newline="") as arquivoCSV:
                     csv_rainhas_conflitos = 0
                     csv_conflitos_linha = 0
                     csv_conflitos_diagonais = 0
+
+                print(
+                    f"[{it + 1}ª Iteração] {csv_tempo} segundos, com pico de {csv_memoria_pico} bytes | [Conflitos] Total: {csv_total_conflitos}, Rainhas: {csv_rainhas_conflitos}, Linhas: {csv_conflitos_linha}, Diagonais: {csv_conflitos_diagonais}")
 
                 writer.writerow(
                     [rainha, funcao.__name__, it + 1, csv_tempo, csv_memoria_pico, csv_total_conflitos, csv_rainhas_conflitos, csv_conflitos_linha, csv_conflitos_diagonais])
